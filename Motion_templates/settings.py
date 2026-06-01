fc91cfe47fef2d1b29da9902c596f4921767bee0
santoshgondkar123 <santoshgondkar22@gmail.com>
2026-06-01 19:05:02 +0530

    Latest changes

--- Motion_templates/settings.py
+++ Motion_templates/settings.py
@@ -186,12 +186,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("santoshgondkar22@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("hkjw ifuh hysd ndnq")
+# EMAIL_HOST_USER = os.getenv("santoshgondkar22@gmail.com")
+# EMAIL_HOST_PASSWORD = os.getenv("hkjw ifuh hysd ndnq")
 
 
-# EMAIL_HOST_USER = "santoshgondkar22@gmail.com"
-# EMAIL_HOST_PASSWORD = "hkjw ifuh hysd ndnq"
+EMAIL_HOST_USER = "santoshgondkar22@gmail.com"
+EMAIL_HOST_PASSWORD = "hkjw ifuh hysd ndnq"
 
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
