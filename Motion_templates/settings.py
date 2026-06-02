66d6311d051feec066506cda8232ea09d2d51ec2
santoshgondkar123 <santoshgondkar22@gmail.com>
2026-06-03 01:32:51 +0530

    add motion-array host

--- Motion_templates/settings.py
+++ Motion_templates/settings.py
@@ -153,10 +153,11 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 DEBUG = True
 ALLOWED_HOSTS = [
-    "motion-templates.onrender.com",
+    "motion-array.onrender.com",
     "motion-templates-1.onrender.com",
     "localhost",
-    "127.0.0.1",]
+    "127.0.0.1",
+]
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
